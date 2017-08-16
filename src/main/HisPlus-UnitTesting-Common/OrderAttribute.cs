63921eed54181f0f7757d7872470c7d4785efd48
dragon.luo <dragon.luo@live.cn>
2017-08-16 18:24:43 +0800

    add test order function

---
+++ src/main/HisPlus-UnitTesting-Common/OrderAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HisPlus.UnitTesting.Common
+{
+    public class OrderAttribute : Attribute
+    {
+        public int I { get; set; }
+
+        public OrderAttribute(int i)
+        {
+            I = i;
+        }
+    }
+}
---
+++ src/main/HisPlus-UnitTesting-Common/TestCaseOrderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HisPlus.UnitTesting.Common
+{
+    [TestCaseOrderer(
+    CustomTestCaseOrderer.TypeName,
+    CustomTestCaseOrderer.AssembyName)]
+    public class TestClassBase
+    {
+        protected static int I;
+
+        protected void AssertTestName(string testName)
+        {
+            var type = GetType();
+            var queue = CustomTestCaseOrderer.QueuedTests[type.FullName];
+            string dequeuedName;
+            var result = queue.TryDequeue(out dequeuedName);
+            Assert.True(result);
+            Assert.Equal(testName, dequeuedName);
+        }
+    }
+}
